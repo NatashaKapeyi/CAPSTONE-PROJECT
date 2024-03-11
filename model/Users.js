db98c51ee1aca4dca947764cda4beb586d9c26c3
NatashaKapeyi <natashazikhona7@gmail.com>
2024-03-11 12:36:01 +0200

    forgot to add ;

--- model/Users.js
+++ model/Users.js
@@ -91,7 +91,7 @@ class Users{
         const {USER_EMAIL, USER_PASSWORD} = req.body
         const Qry = `=SELECT USER_ID,USER_NAME,USER_LASTNAME,USER_AGE,USER_GENDER,USER_ROLE,USER_EMAIL,USER_PASSWORD,USER_PROFILE
         FROM USERS
-        WHERE USER_EMAIL = ${USER_EMAIL}`
+        WHERE USER_EMAIL = ${USER_EMAIL};`
         db.query(Qry, async (error, result)=>{
           if(error) throw error
           if(!result?.length){
