0d712082b421e8f21e6b27d9928b4b834fb17aef
NatashaKapeyi <natashazikhona7@gmail.com>
2024-03-20 10:13:10 +0200

    search input works

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -2,14 +2,14 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <input type="text" placeholder="Search product by name" class="form-control">
+                <input type="text" v-model="searchQry" @input="search" placeholder="Search product by name" class="form-control">
             </div>
             <div class="col">
                 <button class="btn btn-success">Sorting by price</button>
             </div>
         </div>
         <div class="row" v-if="products">
-            <Card v-for="product in products" :key="product.PRODUCT_ID">
+           <Card v-for="product in filterproducts" :key="product.PRODUCT_ID">
                 <template #cardHeader>
                     <h4 class="card-title">{{ product.PRODUCT_NAME }}</h4>
                 </template>
@@ -21,7 +21,7 @@
                         Amount: R{{ product.PRODUCT_PRICE }}
                     </p>
                     <router-link :to="{name: 'product', params: {id: product.PRODUCT_ID}}">View More</router-link>
-                    <p> <a @click="addCart(product)">Add to cart</a></p>
+                    <button><a @click="addCart(product)">Add to cart</a></button>
                 </template>
             </Card>
         </div>
@@ -41,7 +41,20 @@ export default {
     computed:{
         products(){
             return this.$store.state.products
+        },
+        filterproducts() {
+            if (!this.searchQry) {
+                return this.products;
+            }
+            return this.products.filter(product => {
+                return product.PRODUCT_NAME.toLowerCase().includes(this.searchQry.toLowerCase());
+            });
         }
+    },
+    data() {
+        return {
+            searchQry: ''
+        };
     },
     mounted() {
         this.$store.dispatch('retrieveProducts')
@@ -50,7 +63,18 @@ export default {
       addCart(product){
         this.$store.dispatch('addCart', product);
         console.log(`Added ${product.PRODUCT_NAME} to cart.`)
-      }  
+      } ,
+      search() {
+    if (!this.searchQry) {
+        // when nothing is being searched , all products will be shown
+        this.filterproducts = this.products;
+        return;
+    }
+    // Filter products based on the searchQry
+    this.filterproducts = this.products.filter(product => {
+        return product.PRODUCT_NAME.toLowerCase().includes(this.searchQry.toLowerCase());
+    });
+}
     }
 }
 </script>
